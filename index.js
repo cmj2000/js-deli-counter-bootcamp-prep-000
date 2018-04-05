71f98dc9acd190972ff93bb3aec9a6854be83604
c.mj2000@yahoo.com <c.mj2000@yahoo.com>
2018-04-05 00:41:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,113 @@
+var katzDeliLine = [];
+
+
+
+function takeANumber(katzDeliLine)
+{
+  var spot = []
+  var i = 0
+  
+  while(i < katzDeliLine.length)
+     {
+     
+    
+      //spot.push(" "+[i+1]+ ". "+ katzDeliLine[i]   )
+      //return "The line is currently:" +" " + i + ". " + spot + ", "
+      
+    spot.push("Welcome, " + katzDeliLine[i]+". You are numner "+[i+1] + " in line." )
+        i++
+     
+       
+     }
+  
+  
+  
+  
+  
+
+ // for(var i = 0;  i < katzDeliLine.length; i++)
+   
+ if(katzDeliLine.length === 0) 
+  {
+     return  "There is nobody waiting to be served!"
+    
+   }
+
+ {
+   if(spot === katzDeliLine)
+   {
+   spot = katzDeliLine.shift()
+   
+    return "Currently serving " + spot + "."
+       
+    
+   }
+  
+   }
+    
+}
+  
+    
+    
+    
+  
+  
+  
+  
+
+////////////////////////////////////got right////////////////
+
+function nowServing(katzDeliLine)
+{
+   var spot = []
+ if(katzDeliLine.length === 0) 
+  {
+     return  "There is nobody waiting to be served!"
+    
+   }
+ else
+ {
+   spot = katzDeliLine.shift()
+   
+    return "Currently serving " + spot + "."
+       
+    }
+    
+  }
+ 
+  /////////////////////////////////////////////////////////////
+  
+  ////////////////////////////////////////got right///////////
+  function currentLine(katzDeliLine)
+  {
+     
+    
+   
+      var spot = []
+      var i = 0;
+    while(i < katzDeliLine.length)
+     {
+     
+    
+      spot.push(" "+[i+1]+ ". "+ katzDeliLine[i]   )
+      //return "The line is currently:" +" " + i + ". " + spot + ", "
+      
+      
+        i++
+     
+       
+     }
+     
+     if(katzDeliLine.length === 0) 
+     {
+       return  "The line is currently empty."
+    
+     }
+     
+     else
+     return  "The line is currently:" + spot +""
+  
+  }
+  
+  //////////////////////////////////////////////////////////////
+  
